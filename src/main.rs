e63494abfa04540ab86e5542b2e3ac6cb471bbe0
Eugene <gish.ee18@gmail.com>
2021-04-18 11:19:52 +0300

    d

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+struct List {
+    head: Link,
+}
+
+#[derive(Debug)]
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+#[derive(Debug)]
+struct Node {
+    data: Box<str>,
+    next: Link,
+}
+
+impl List {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = Box::from(Node{data : Box::from("hi"), prev: None, next: None});
+    let mut b = Node{data: Box::from("aa"), prev:None, next: Some(a)};
+    a.next = Some(Box::from(b));
+    println!("{:?}", a);
 }
