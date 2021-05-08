57c6aa881d29a92dcdd0e6268e6cda7eddde86d9
Eugene <fogaprod@gmail.com>
2021-05-08 18:01:47 +0300

    Cleanup by moving operand code to functions, thanks @saraph

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -26,59 +26,106 @@ impl Interpreter {
             }),
         }
     }
+
+    fn operands_subtract(
+        left: &Object,
+        right: &Object,
+        operator: &Token,
+    ) -> anyhow::Result<Object> {
+        Ok(Object::Number(
+            Self::operand_into_number(operator, &left)?
+                - Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_divide(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Number(
+            Self::operand_into_number(operator, &left)?
+                / Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_multiply(
+        left: &Object,
+        right: &Object,
+        operator: &Token,
+    ) -> anyhow::Result<Object> {
+        Ok(Object::Number(
+            Self::operand_into_number(operator, &left)?
+                * Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_add(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(match (&left, &right) {
+            (Object::Number(number1), Object::Number(number2)) => Object::Number(number1 + number2),
+            (Object::String(string1), Object::String(string2)) => {
+                Object::String(string1.to_owned() + string2)
+            }
+            _ => anyhow::bail!(RuntimeError {
+                token: operator.clone(),
+                message: "Operands must be two numbers or two strings.".to_string()
+            }),
+        })
+    }
+
+    fn operands_cmp_gt(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(
+            Self::operand_into_number(operator, &left)?
+                > Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_cmp_ge(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(
+            Self::operand_into_number(operator, &left)?
+                >= Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_cmp_lt(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(
+            Self::operand_into_number(operator, &left)?
+                < Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_cmp_le(left: &Object, right: &Object, operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(
+            Self::operand_into_number(operator, &left)?
+                <= Self::operand_into_number(operator, &right)?,
+        ))
+    }
+
+    fn operands_cmp_ne(left: &Object, right: &Object, _operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(left == right))
+    }
+
+    fn operands_cmp_eq(left: &Object, right: &Object, _operator: &Token) -> anyhow::Result<Object> {
+        Ok(Object::Boolean(left != right))
+    }
 }
 
 impl Visitor<anyhow::Result<Object>> for Interpreter {
     fn visit_assign(&self, name: &Token, value: &Expr) -> anyhow::Result<Object> {
         todo!();
     }
+
     fn visit_binary(&self, left: &Expr, operator: &Token, right: &Expr) -> anyhow::Result<Object> {
-        let left = self.evaluate(left)?;
-        let right = self.evaluate(right)?;
+        let left = &self.evaluate(left)?;
+        let right = &self.evaluate(right)?;
 
         Ok(match operator.token_type {
-            TokenType::Minus => Object::Number(
-                Self::operand_into_number(operator, &left)?
-                    - Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::Slash => Object::Number(
-                Self::operand_into_number(operator, &left)?
-                    / Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::Star => Object::Number(
-                Self::operand_into_number(operator, &left)?
-                    * Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::Plus => match (&left, &right) {
-                (Object::Number(number1), Object::Number(number2)) => {
-                    Object::Number(number1 + number2)
-                }
-                (Object::String(string1), Object::String(string2)) => {
-                    Object::String(string1.to_owned() + string2)
-                }
-                _ => anyhow::bail!(RuntimeError {
-                    token: operator.clone(),
-                    message: "Operands must be two numbers or two strings.".to_string()
-                }),
-            },
-            TokenType::Greater => Object::Boolean(
-                Self::operand_into_number(operator, &left)?
-                    > Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::GreaterEqual => Object::Boolean(
-                Self::operand_into_number(operator, &left)?
-                    >= Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::Less => Object::Boolean(
-                Self::operand_into_number(operator, &left)?
-                    < Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::LessEqual => Object::Boolean(
-                Self::operand_into_number(operator, &left)?
-                    <= Self::operand_into_number(operator, &right)?,
-            ),
-            TokenType::BangEqual => Object::Boolean(left != right),
-            TokenType::EqualEqual => Object::Boolean(left == right),
+            TokenType::Minus => Self::operands_subtract(left, right, operator)?,
+            TokenType::Slash => Self::operands_divide(left, right, operator)?,
+            TokenType::Star => Self::operands_multiply(&left, right, operator)?,
+            TokenType::Plus => Self::operands_add(left, right, operator)?,
+            TokenType::Greater => Self::operands_cmp_gt(left, right, operator)?,
+            TokenType::GreaterEqual => Self::operands_cmp_ge(left, right, operator)?,
+            TokenType::Less => Self::operands_cmp_lt(left, right, operator)?,
+            TokenType::LessEqual => Self::operands_cmp_le(left, right, operator)?,
+            TokenType::BangEqual => Self::operands_cmp_ne(left, right, operator)?,
+            TokenType::EqualEqual => Self::operands_cmp_eq(left, right, operator)?,
             _ => unreachable!(),
         })
     }
